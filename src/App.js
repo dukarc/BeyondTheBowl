c12254a812c6bc7b5f37f47372a5c87aae08d45f
cyrus <cyrus.dukart@gmail.com>
2018-03-21 22:51:12 -0700

    added material-ui

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Wootville</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <MuiThemeProvider>
+          <Card>
+            <CardMedia
+              overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle" />}
+            >
+              <img src="https://upload.wikimedia.org/wikipedia/commons/4/44/Jelly_cc11.jpg" alt="" />
+            </CardMedia>
+            <CardTitle title="Card title" subtitle="Card subtitle" />
+            <CardText>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+              Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+              Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+            </CardText>
+          </Card>
+        </MuiThemeProvider>
       </div>
     );
   }
